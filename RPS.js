94e2d22fc0c4ee437ff230593e3e8d7c829fc896
Lilracko <170021881+Lilracko@users.noreply.github.com>
2024-06-17 13:01:48 -0400

    added a round system and the win conditions

--- RPS.js
+++ RPS.js
@@ -8,14 +8,50 @@ function getComputerChoice(){
    case 2:
     return 'scissors';  
 }
+const computerchoice = getComputerChoice();
 }
-const computerchoice = getComputerChoice();{
-function getHumanChoice
-    let choice = prompt("Eneter your choice:rock, paper, or scissors").toLowerCase();
-    while (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors'){
-        choice = prompt("Invalid choice! Please enter rock, paper, or scissors.").toLowerCase
+function getHumanChoice() {
+    let choice = prompt("Enter your choice: rock, paper, or scissors").toLowerCase();
+
+    while (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
+        choice = prompt("Invalid choice! Please enter rock, paper, or scissors.").toLowerCase();
     }
+
     return choice;
+}
+const humanChoice = getHumanChoice();
+let humanscore= 0;
+let computerscore =0;
+
+function playRound(humanChoice, computerchoice){
+    humanChoice = humanChoice.toLowerCase();
+
+    if (
+        (humanChoice === 'rock' && computerchoice === 'scissors') ||
+        (humanChoice === 'paper' && computerchoice === 'rock') ||
+        (humanChoice === 'scissors' && computerchoice === 'paper')
+    ){
+        humanscore+++
+        console.log('You Win! ${humanChoice} beats ${computerChoice}');
     }
-    const humanchoice = getHumanChoice();
-    console.log('Human Choice',humanchoice)
+    else if (
+        (computerChoice === 'rock' && humanChoice === 'scissors') ||
+        (computerChoice === 'paper' && humanChoice === 'rock') ||
+        (computerChoice === 'scissors' && humanChoice === 'paper')
+    ) 
+    {
+        computerscore+++
+        console.log('You lose! ${computerChoice} beats ${humanChoice}');   
+    }
+    else {
+        console.log("It's a tie! Both chose ${humanchoice}");
+    }
+    console.log('Scores- You: ${humanscore} | Computer: ${computerscore}');
+}
+function playGame(){
+    playRound();
+    for (let i =0; i<5; i++) {
+        const humanChoice=getHumanChoice();
+        const  computerChoice = getComputerChoice();
+    }
+}
